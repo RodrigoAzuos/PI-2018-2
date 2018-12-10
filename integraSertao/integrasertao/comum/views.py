936f0f30bfadc951f85cada6a5891f79d5fadabe
José Vinícius <lopesvinicius1707@gmail.com>
2018-12-10 11:20:44 -0300

    Adição de Layout em Login, PostDetalhado e ícones.

--- integraSertao/integrasertao/comum/views.py
+++ integraSertao/integrasertao/comum/views.py
@@ -42,7 +42,7 @@ def add_post(request):
 
 def exibir_post(request, post_id):
     post = Post.objects.get(pk=post_id)
-    return render(request, "post_detalhado.html", {'post': post})
+    return render(request, "post.html", {'post': post})
 
 def index2(request):
     principal = Post.objects.all().order_by('-criado_em')[:3]
--- integraSertao/integrasertao/usuario/urls.py
+++ integraSertao/integrasertao/usuario/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     ##Usuario##
     path('registrar/', views.RegistrarUsuarioView.as_view() , name = 'registrar'),
     path('login/', login, {'template_name': 'login.html'}, name='login'),
-	path('logout/', logout_then_login, {'login_url': 'login'},name= 'logout'),
+    path('login2/', login, {'template_name': 'login2.html'}, name='login2'),
+    path('logout/', logout_then_login, {'login_url': 'login'},name= 'logout'),
 ]
